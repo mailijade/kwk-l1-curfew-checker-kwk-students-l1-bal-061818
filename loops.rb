c09d6dfee9d98d252eab74fe5019bdf965e51716
mailijade <mailijade23@gmail.com>
2018-06-20 13:40:44 +0000

    automatically backed up by learn

---
+++ loops.rb
@@ -0,0 +1,12 @@
+def whip_and_nea_nea
+  loop do
+    puts "now watch me whip"
+    puts "now watch me nea nea"
+    puts "oooo watch me whip,whip"
+    puts "now whatch me nea nea"
+    puts "oooo whatch me, whatch me"
+  end 
+end
+
+whip_and_nea_nea 
+  
